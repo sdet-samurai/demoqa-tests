dd5de68a41699fbd3b4b7dc8eca98b84499a25c0
larginator <larginator@gmail.com>
2024-05-18 23:27:12 +0600

    Replace XPath selectors with CSS selectors

--- src/test/java/com/demoqa/SimpleJUnitTest.java
+++ src/test/java/com/demoqa/SimpleJUnitTest.java
@@ -1,26 +1,26 @@
 package com.demoqa;
 
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
 
-import static com.codeborne.selenide.Selectors.byXpath;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class SimpleJUnitTest {
     @BeforeAll
     static void config() {
         Configuration.baseUrl = "https://demoqa.com/";
-        Configuration.headless = true;
         Configuration.pageLoadStrategy = "eager";
         Configuration.browserSize = "1920x1080";
+        Configuration.headless = true;
     }
 
     @Test
-    void formSubmissionWithAllFieldsFilled() {
+    void formSubmissionWithAllFieldsFilledTest() {
         // Arrange
         String firstName = "FirstName";
         String lastName = "LastName";
@@ -39,37 +39,39 @@ public class SimpleJUnitTest {
 
         // Act
         open("automation-practice-form");
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
         $("#firstName").setValue(firstName);
         $("#lastName").setValue(lastName);
         $("#userEmail").setValue(email);
-        $x(String.format("//label[contains(text(), '%s')]", gender)).click();
+        $("#genterWrapper").$(byText(gender)).click();
         $("#userNumber").setValue(phoneNumber);
         $("#dateOfBirthInput").click();
         $("select.react-datepicker__month-select").selectOption(monthOfBirth);
         $("select.react-datepicker__year-select").selectOption(yearOfBirth);
-        $x(String.format("//div[@class='react-datepicker__month-container']//div[contains(text(), '%s')]", dayOfBirth)).click();
+        $(".react-datepicker__day--011").click();
         $("input#subjectsInput").setValue(subject).pressEnter();
-        $("#hobbiesWrapper").$(byXpath(String.format(".//*[text()='%s']", hobby))).click();
+        $("#hobbiesWrapper").$(byText(hobby)).click();
         $("#uploadPicture").uploadFile(picture);
         $("#currentAddress").setValue(address);
         $("#state").click();
-        $x(String.format("//*[@id='state']//*[text()='%s']", state)).click();
+        $("#state").$(byText(state)).click();
         $("#city").click();
-        $x(String.format("//*[@id='city']//*[text()='%s']", city)).click();
+        $("#city").$(byText(city)).click();
         $("#submit").submit();
 
         // Assert
-        $(".table-responsive").shouldHave(Condition.text(firstName + " " + lastName));
-        $(".table-responsive").shouldHave(Condition.text(email));
-        $(".table-responsive").shouldHave(Condition.text(gender));
-        $(".table-responsive").shouldHave(Condition.text(phoneNumber));
+        $(".table-responsive").shouldHave(text(firstName + " " + lastName));
+        $(".table-responsive").shouldHave(text(email));
+        $(".table-responsive").shouldHave(text(gender));
+        $(".table-responsive").shouldHave(text(phoneNumber));
         $(".table-responsive").shouldHave(
-                Condition.text(formatDateOfBirth(dayOfBirth, monthOfBirth, yearOfBirth)));
-        $(".table-responsive").shouldHave(Condition.text(subject));
-        $(".table-responsive").shouldHave(Condition.text(hobby));
-        $(".table-responsive").shouldHave(Condition.text(picture.getName()));
-        $(".table-responsive").shouldHave(Condition.text(address));
-        $(".table-responsive").shouldHave(Condition.text(state + " " + city));
+                text(formatDateOfBirth(dayOfBirth, monthOfBirth, yearOfBirth)));
+        $(".table-responsive").shouldHave(text(subject));
+        $(".table-responsive").shouldHave(text(hobby));
+        $(".table-responsive").shouldHave(text(picture.getName()));
+        $(".table-responsive").shouldHave(text(address));
+        $(".table-responsive").shouldHave(text(state + " " + city));
     }
 
     private String formatDateOfBirth(String day, String month, String year) {
